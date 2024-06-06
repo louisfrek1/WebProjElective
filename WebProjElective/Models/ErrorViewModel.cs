b7996908b71a70dba1d8a6f1e8f6ef7d65b3f47c
louisfrek1 <louise.ranz143@gmail.com>
2024-06-06 14:03:12 +0800

    Add project files.

---
+++ WebProjElective/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebProjElective.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
